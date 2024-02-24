034dbc6ff7b4a96d406bef895d8e3d3bdd2f3a2e
lawrence-254 <lodhiambo26@gmail.com>
2024-02-24 13:59:36 +0300

    stringFuncs

--- Program.cs
+++ Program.cs
@@ -7,6 +7,19 @@ namespace urban_fortnight
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            string today = "    monday     ";
+
+            Console.WriteLine($"Today is {today}");
+
+            string noWhiteSpace = today.Trim();
+
+            Console.WriteLine($"Today is {noWhiteSpace}");
+
+            string changeDay = today.Replace("monday", "tuesday");
+
+            Console.WriteLine($"Today is {changeDay}");
+
         }
     }
 }
