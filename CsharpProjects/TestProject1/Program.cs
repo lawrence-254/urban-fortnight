13c472b393252a95178cc08daed6ecd4e82faf76
lawrence-254 <lodhiambo26@gmail.com>
2024-02-25 16:36:05 +0300

    step2

--- CsharpProjects/TestProject1/Program.cs
+++ CsharpProjects/TestProject1/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
+
+// Your code goes here
+if (daysUntilExpiration <= 10)
+{
+    if (daysUntilExpiration <= 5)
+    {
+        Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
+        Console.WriteLine("Renew now and save 10%!");
+    }
+    else if (daysUntilExpiration <= 1)
+    {
+        Console.WriteLine("Your subscription expires within a day!");
+        Console.WriteLine("Renew now and save 20%!");
+    }
+    else if (daysUntilExpiration == 0)
+    {
+        Console.WriteLine("Your subscription has expired.");
+    }
+    else
+    {
+        Console.WriteLine("Your subscription will expire soon. Renew now!");
+    }
+}
+if (discountPercentage > 0)
+{
+    Console.WriteLine($"Renew now and save {discountPercentage}%.");
+}
