441255c320c1aa0326fc9961c04958d7ceacd194
lawrence-254 <lodhiambo26@gmail.com>
2024-02-25 21:03:28 +0300

    step5

--- CsharpProjects/TestProject4/Program.cs
+++ CsharpProjects/TestProject4/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] myArray = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"};
+
+foreach(string item in myArray)
+{
+    if item.StartsWith("B")
+    {
+        Console.WriteLine($"{item}");
+    }
+
+
+}
